4098804d2db08ed0d68049ae6e3115f82138a74e
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-03-25 19:34:22 -0300

    generating and retrieving image

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,17 @@ dotenv.config();
 import express from "express"
 import ImageGenerator from "./handlers/replicate/replicate.js";
 
+import generateImage from "./routes/generateImage.js";
+import getPrediction from "./routes/getPrediction.js";
+
 const token = process.env.TOKEN
 const modelId = process.env.MODEL_ID
 
 const app = express();
 
 
-app.get('/', (req, res) => {
-    const imageCreator = new ImageGenerator({ token, modelId });
-    imageCreator.runModel("a medieval castle in the middle of a forest, higly detailed, with a river flowing through it, Unreal, ArtStation")
-        .then((response) => {
-            console.log(response.data)
-            res.send(response.data)
-        })
-        .catch((e) => {
-            console.log(e)
-            res.status(500).send(e)
-        })
-})
+app.post('/generate', generateImage)
+app.get('/output', getPrediction)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`server on http://localhost:${port}`) })
--- src/routes/getPrediction.ts
+++ src/routes/getPrediction.ts
@@ -7,16 +7,16 @@ import ImageGenerator from "../handlers/replicate/replicate.js";
 export default function generateImage(req: Request, res: Response) {
     const token = process.env.TOKEN;
     const modelId = process.env.MODEL_ID;
-    const prompt = req.body.prompt;
+    const id = req.query.id as string;
 
     const imageCreator = new ImageGenerator({ token, modelId });
-    imageCreator.runModel("a medieval castle in the middle of a forest, higly detailed, with a river flowing through it, Unreal, ArtStation")
+
+    imageCreator.predictionOutput(id)
         .then((response) => {
-            console.log(response.data);
-            res.send(response.data);
+            console.log(response);
+            res.send(response);
         })
         .catch((e) => {
-            console.log(e);
             res.status(500).send(e);
-        });
+        })
 }
